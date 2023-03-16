0df503c3f5e55f10a3f075bbadd6054447108f8e
Andrea <andrea@yaaaw.it>
2023-03-16 15:48:54 +0100

    refactor: update notbyai badge

--- app/root.tsx
+++ app/root.tsx
@@ -33,8 +33,8 @@ export default function App() {
         <div className="static sm:relative">
           <Outlet />
           <div className="fixed m-3 bottom-0 right-0">
-            <a href="https://notbyai.fyi/">
-              <img src="https://user-images.githubusercontent.com/62137266/225637256-6b497779-d10e-40be-8c4a-e0e6349d7b25.svg" />
+            <a href="https://notbyai.fyi/" target="_blank">
+              <img src="https://user-images.githubusercontent.com/62137266/225653923-a69103f5-b318-4e52-9ea1-95b61d388366.svg" />
             </a>
           </div>
         </div>
