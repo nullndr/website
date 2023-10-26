e832a0ecdbcce603058bf94e8841a7385194f07e
Andrea <andrea@yaaaw.it>
2023-10-26 12:14:12 +0200

    chore: add code hike to remark plugins

--- remix.config.js
+++ remix.config.js
@@ -1,7 +1,13 @@
+const { remarkCodeHike } = require("@code-hike/mdx");
+const theme = require("shiki/themes/one-dark-pro.json");
+
 /** @type {import('@remix-run/dev').AppConfig} */
 module.exports = {
   ignoredRouteFiles: ["**/.*"],
   serverDependenciesToBundle: ["mdx-bundler"],
   tailwind: true,
   serverModuleFormat: "cjs",
+  mdx: {
+    remarkPlugins: [[remarkCodeHike, { theme }]],
+  },
 };
