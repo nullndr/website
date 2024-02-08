10fe0add4a7a2c3f4f8cf5f56fc0015395676211
Andrea <andrea@yaaaw.it>
2024-02-08 08:48:35 +0100

    chore: remove polyfills

--- remix.config.ts
+++ remix.config.ts
@@ -3,27 +3,4 @@ import type { AppConfig } from "@remix-run/dev";
 export default {
   ignoredRouteFiles: ["**/.*"],
   tailwind: true,
-  browserNodeBuiltinsPolyfill: {
-    modules: {
-      path: true,
-      fs: true,
-      vm: true,
-      util: true,
-      buffer: true,
-      string_decoder: true,
-      events: true,
-      os: true,
-      crypto: true,
-      http: true,
-      child_process: true,
-      https: true,
-      url: true,
-      net: true,
-      tls: true,
-      assert: true,
-      stream: true,
-      zlib: true,
-      punycode: true,
-    },
-  },
 } satisfies AppConfig;
