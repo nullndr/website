2206f02e2af165e894971827e1c605dc67d4f01c
Andrea <andrea@yaaaw.it>
2024-02-08 08:26:56 +0100

    chore: remove mdx-bundler from server dependencies bundle

--- remix.config.ts
+++ remix.config.ts
@@ -2,7 +2,6 @@ import type { AppConfig } from "@remix-run/dev";
 
 export default {
   ignoredRouteFiles: ["**/.*"],
-  serverDependenciesToBundle: ["mdx-bundler"],
   tailwind: true,
   browserNodeBuiltinsPolyfill: {
     modules: {
