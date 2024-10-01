af931b764c0582b3292db7a5cd446cdef58b63b9
Andrea <andrea@yaaaw.it>
2024-10-01 13:46:53 +0200

    fix(formatted date): show month and date with two digits

--- app/hooks/useFormattedDate.ts
+++ app/hooks/useFormattedDate.ts
@@ -1,4 +1,6 @@
 export function useFormattedDate(_date: string) {
-  const date = new Date(_date)
-  return `${date.getUTCFullYear()}/${date.getUTCMonth()}/${date.getUTCDate()}`
+  const date = new Date(_date);
+  const month = date.getUTCMonth().toString().padStart(2, "0");
+  const monthDate = date.getUTCDate().toString().padStart(2, "0");
+  return `${date.getUTCFullYear()}/${month}/${monthDate}`;
 }
